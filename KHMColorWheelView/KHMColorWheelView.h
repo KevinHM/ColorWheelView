d4b411979eea20ccbc4dde3b598528b3ea5041ea
DarkHorse1919 <xiaohm1983@gmail.com>
2015-08-08 13:19:51 +0800

    Move KHMColorWheelView To Root Dir

---
+++ KHMColorWheelView/KHMColorWheelView.h
@@ -0,0 +1,17 @@
+//
+//  KHMColorWheelView.h
+//
+//  Created by KevinHM on 15/8/8.
+//  Copyright (c) 2015年 KevinHM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHMColorWheelView : UIView
+@property (nonatomic, readonly, strong) UIColor *currentColor; /*!< 当前颜色*/
+@property (nonatomic, assign) BOOL continueReponse; /*!< 是否持续响应事件*/
+
+@property (nonatomic, copy) void (^colorDidChangedBlock)(UIColor *color); /*!< 颜色变化回调*/
+@property (nonatomic, copy) void (^colorEventBlock)(UIColor *color); /*!< 颜色相关事件回调*/
+
+@end
